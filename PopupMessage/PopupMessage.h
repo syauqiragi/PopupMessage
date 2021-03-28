b34ea815a253e078b2d52fd9c97b36a96b6f6d42
syauqi <ahmad@mamiteam.com>
2021-03-28 11:05:46 +0700

    Initial project setup

---
+++ PopupMessage/PopupMessage.h
@@ -0,0 +1,18 @@
+//
+//  PopupMessage.h
+//  PopupMessage
+//
+//  Created by Ahmad Syauqi Albana on 28/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopupMessage.
+FOUNDATION_EXPORT double PopupMessageVersionNumber;
+
+//! Project version string for PopupMessage.
+FOUNDATION_EXPORT const unsigned char PopupMessageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupMessage/PublicHeader.h>
+
+
